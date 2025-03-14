2d21c691b15906f736c9a5f85ae1c52d32c7479f
Tomas Rivero <trivero@oficinacys.com.ar>
2025-03-14 00:19:13 -0300

    Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,13 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 import Providers from "./providers"; // Importa el componente Providers
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="">
-
+      <Analytics/>
         {/* Envuelve toda la aplicación con el componente Providers */}
         <Providers>
           <Navbar />
